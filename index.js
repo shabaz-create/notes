3a37c050d47e2e1b8e8a53debe7e96530a7c13a7
shabaz-create <shabaz.jnv@gmail.com>
2024-06-05 08:12:36 +0530

    delete functionality working, archive & pinning yet to go

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let navbar=document.querySelector("#navbar");
 let notesContainer=document.querySelector(".notes-container");
 let notesArchiveContainer=document.querySelector(".notes-archive-container")
-let notesEditorContainer=document.querySelector("#notes-editor-container");
+let notesEditorContainer=document.querySelector(".notes-editor-container");
 let notesTitle=document.querySelector("#notes-title");
-let notes=document.querySelector("#notes");
+let notes=document.querySelector("#notes-content");
 let notesArray=JSON.parse(localStorage.getItem("notesArray"))||[];
+
+// let container=document.querySelector(".container");
+renderingNotes(notesArray);
+// let button=document.querySelector(".hidden-buttons")
 
 
 //home & archive clicking works now
@@ -29,7 +33,8 @@ notesEditorContainer.addEventListener("click",(event)=>{
     let title=notesTitle.value.trim();
 
     if(contentKey==="submit" && content!="" && title!=""){
-        notesArray=[...notesArray,{title:title,content:content}]
+        let id=Date.now();
+        notesArray=[...notesArray,{id:id,title:title,content:content}]
         notes.value=notesTitle.value="";
         console.log(notesArray);
         localStorage.setItem("notesArray",JSON.stringify(notesArray));
@@ -38,15 +43,37 @@ notesEditorContainer.addEventListener("click",(event)=>{
     
 })
 
+notesContainer.addEventListener("click",(event)=>{
+    let key=event.target.dataset.key;
+    let id=event.target.parentNode.parentNode.id;
+    if(key==="delete"){
+        notesArray=notesArray.filter((element)=>element.id.toString()!=id);
+        localStorage.setItem("notesArray",JSON.stringify(notesArray));
+    }
+
+    renderingNotes(notesArray);
+})
+
 function renderingNotes(notes){
-    notesContainer.innerHTML=notes.map(({title,content})=>
-        `<div class="item">
+    notesContainer.innerHTML=notes.map(({title,content,id=Date.now()})=>
+        `<div class="item" id=${id}>
+            <span class="item-delete-button-container">
             <h1>${title}</h1>
+            <button class="hidden-buttons" data-key="delete">delete</button>
+            </span>
+            <div class="item-extra-buttons-container">
             <p>${content}</p>
+            <span>
+            <button class="" data-key="pin">pin</button>
+            <button class="" data-key="archive">archive</button>
+            </span>
+            </div>
          </div>`
     )
 
 }
+
+
 
 
 renderingNotes(notesArray);
